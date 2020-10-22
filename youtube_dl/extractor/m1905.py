a36358690ab6e3c6bcf992096849f85786c07752
liubin <jinzuchi@163.com>
2020-10-22 17:43:43 +0800

    m1905下载

--- youtube_dl/extractor/m1905.py
+++ youtube_dl/extractor/m1905.py
@@ -10,6 +10,8 @@ from ..compat import (
     compat_HTTPError,
     compat_str,
     compat_urllib_parse_urlencode,
+    compat_urllib_parse_unquote_plus,
+    compat_urllib_parse
 )
 from ..utils import (
     ExtractorError,
@@ -26,19 +28,19 @@ class M1905IE(InfoExtractor):
         video_id = self._match_id(url)
         try:
             params = {
-                'nonce': '1603288973',
-                'expiretime': '1603289573',
-                'cid': '1485702',
+                'nonce': int(time.time()),
+                'expiretime': int(time.time())+600,
+                'cid': video_id,
                 'uuid': '09699053-4b9c-40a9-9231-908ee8a1f5b2',
                 'playerid': '897249252991526',
-                'page': 'https://www.1905.com/video/play/1485702.shtml',
+                'page': url,
                 'type': 'mp4'
             }
             params['signature'] = self._signature(params, 'dde3d61a0411511d')
-            self.to_screen('params %s' % params)
-            api_data = self._download_json(
-                'https://profile.m1905.com/mvod/getVideoinfo.php', video_id, query=params)['data']
-            self.to_screen('api_data %s' % api_data)
+            params['callback'] = 'fnCallback0'
+            content, urlh = self._download_webpage_handle('https://profile.m1905.com/mvod/getVideoinfo.php', video_id, query=params)
+            content = content[12:-1]
+            api_data = self._parse_json(content, video_id)['data']
         except ExtractorError as e:
             if isinstance(e.cause, compat_HTTPError) and e.cause.code == 401:
                 error = self._parse_json(e.cause.read().decode(), None)
@@ -46,12 +48,30 @@ class M1905IE(InfoExtractor):
                     self.raise_geo_restricted(countries=self._GEO_COUNTRIES)
                 raise ExtractorError(error['msg'], expected=True)
             raise
+        quality = ['sd', 'hd', 'uhd']
+        formats = []
+        for q in quality:
+            formats.append({
+                'format_id': compat_str(q),
+                'url': api_data['quality'][q]['host']+api_data['sign'][q]['sign']+api_data['path'][q]['path'],
+                'ext': 'mp4',
+                'protocol': 'https',
+                'http_headers': {
+                    'Referer': url,
+                }
+            })
+        self._sort_formats(formats)
+        return {
+            'id': video_id,
+            'title': api_data['title'],
+            'formats': formats
+        }
+
 
     def _signature(self, p, appid):
         s = ''
         for k in sorted(p):
             if len(s) > 0:
                 s += '&'
-            s += (k + '=' + compat_str(p[k]))
-        self.to_screen(s)
+            s += (k + '=' + compat_urllib_parse.quote_plus(compat_str(p[k])))
         return hashlib.sha1((s + '.' + appid).encode()).hexdigest()
